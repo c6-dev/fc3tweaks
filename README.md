138cafe225f63f2b3491b56928d5362bcf7302d4
c6 <31777460+c6-dev@users.noreply.github.com>
2023-11-17 17:40:26 +0400

    add remove online popup

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Far Cry 3 Tweaks
 Includes:
 * Skip fast travel pop-up confirmation  
-
+* Remove "Online servers are not available anymore" error  
 
 Credits to Silent and ThirteenAG for the [pattern-matching library](https://github.com/CookiePLMonster/ModUtils/).
--- patches.h
+++ patches.h
@@ -8,5 +8,8 @@ void writePatches() {
 	}
 	auto skipFastTravelConfirmation = hook::make_module_pattern(module, "24 0F 00 80 7C 24 0D 00").get_first(8);
 	SafeWrite8((UInt32)skipFastTravelConfirmation, 0x75);
+	auto removeOnlinePopup = hook::make_module_pattern(module, "50 C7 45 90 02 00 00 00").get_first(8);
+	PatchMemoryNop((UInt32)removeOnlinePopup, 7);
+
 
 }
